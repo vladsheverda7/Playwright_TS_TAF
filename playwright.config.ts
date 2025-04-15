6c31ee82435973e3a5f9bb0f43be66fdbd8ef252
vlad <vladsheverda7@gmail.com>
2025-04-15 12:33:50 +0400

    Update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,46 +1,29 @@
 import { defineConfig, devices } from '@playwright/test';
 import { TestOptions } from './test-options';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig<TestOptions>({
     timeout: 30000,
     globalTimeout: 60000,
     expect: {
         timeout: 2000,
     },
-    testDir: './tests',
-    /* Run tests in files in parallel */
-    fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 1,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 4,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        //baseURL: 'http://localhost:4200/',
-        globalQAURL: 'https://www.globalsqa.com/demo-site/draganddrop/',
-        baseURL:
-            process.env.DEV === '1' ? 'http://localhost:4200/' : process.env.STAGING == '1' ? 'http://localhost:4202/' : 'http://localhost:4201/',
 
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    testDir: './tests',
+    fullyParallel: true,
+    retries: 1,
+    reporter: 'html',
+
+    use: {
+        globalQAURL: 'https://www.globalsqa.com/demo-site/draganddrop/',
+        baseURL: process.env.DEV == '1' ? 'http://localhost:4201/' : process.env.STAGING == '1' ? 'http://localhost:4202/' : 'http://localhost:4200/',
+
         trace: 'on',
         video: {
-            mode: 'on',
+            mode: 'off',
             size: {
                 width: 1920,
                 height: 1080,
@@ -50,16 +33,10 @@ export default defineConfig<TestOptions>({
         //navigationTimeout: 5000,
     },
 
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'dev',
             use: { ...devices['Desktop Chrome'], baseURL: 'http://localhost:4201/' },
-            fullyParallel: true,
-        },
-        {
-            name: 'staging',
-            use: { ...devices['Desktop Chrome'], baseURL: 'http://localhost:4202/' },
             fullyParallel: true,
         },
         {
@@ -70,39 +47,31 @@ export default defineConfig<TestOptions>({
 
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
+            use: { browserName: 'firefox' },
         },
 
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
+            use: {
+                browserName: 'webkit',
+                video: {
+                    mode: 'on',
+                    size: {
+                        width: 1920,
+                        height: 1080,
+                    },
+                },
+            },
         },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
+        {
+            name: 'pageObjectFullScreen',
+            testMatch: 'usePageObject.spec.ts',
+            use: {
+                viewport: {
+                    width: 1920,
+                    height: 1080,
+                },
+            },
+        },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
 });
